2292d2d021db348a0358d0f3461bed12dc228c01
Shubhansh16 <shubhansh77@gmail.com>
2024-08-02 16:48:01 +0530

    "first commit"

---
+++ app/src/main/java/com/example/jetpack1/MainActivity.kt
@@ -0,0 +1,48 @@
+mpackage com.example.jetpack1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpack1.ui.theme.Jetpack1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Jetpack1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+jVXGC FHJK;'/,'@Preview(showBackground = true)
+>|
+@Composable
+fun GreetingPreview() {
+    Jetpack1Theme {
+        Greeting("Android")
+    }
+}
